64233d7a217b81814f5a4aca0f403782430b7b66
DaveDanDev <davedevs0121@gmail.com>
2025-02-15 20:52:15 +0800

    Save data and generate HTML for payment summary

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -1,10 +1,10 @@
 // when importing modules "./" -  means current folder
 import {cart,removeFromCart, updateDeliveryOption} from '../../data/cart.js';
-import {products} from '../../data/products.js';
+import {products, getProduct} from '../../data/products.js';
 import {formatCurrency} from '../utils/money.js';
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
 //copy the url of the esm version of external library for the import path
-import {deliveryOptions} from '../../data/deliveryOptions.js';
+import {deliveryOptions, getDeliveryOption} from '../../data/deliveryOptions.js';
 
 /*
 //today's date
@@ -20,23 +20,13 @@ export function renderOrderSummary() {
   cart.forEach((cartItem) => {
     const productId = cartItem.productId;
 
-    let matchingProduct = '';
+    const matchingProduct = getProduct(productId);
 
-    products.forEach((product) => {
-      if(product.id === productId) {
-        matchingProduct = product;
-      } 
-    });
     const deliveryOptionId = cartItem.deliveryOptionId;
 
-    let deliveryOption = '';
+    const deliveryOption =getDeliveryOption(deliveryOptionId);
 
-    deliveryOptions.forEach((option) => {
-      if(option.id === deliveryOptionId){
-        deliveryOption = option;
-      }
-      
-      });
+   
 
     const today = dayjs();
     const deliveryDate = today.add(
