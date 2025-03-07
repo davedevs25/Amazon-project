b7eb48ceb1613dbf6d52ff89520fa20a90cb990e
DaveDanDev <davedevs0121@gmail.com>
2025-03-08 00:43:57 +0800

    Create oop version of cart

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,6 +1,7 @@
 import {cart,addToCart,getCartQuantity} from '../data/cart.js';
 import {products} from '../data/products.js';
 import { formatCurrency } from './utils/money.js';
+import '../data/cart-oop.js';
 
 export function renderAmazon(){
 updateCartQuantity();
