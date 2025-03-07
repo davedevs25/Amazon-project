fa48b30e016a04c8f2edf9987d9d2d6f766ab53f
DaveDanDev <davedevs0121@gmail.com>
2025-03-08 01:02:07 +0800

    create class version of cart

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,7 +1,7 @@
 import {cart,addToCart,getCartQuantity} from '../data/cart.js';
 import {products} from '../data/products.js';
 import { formatCurrency } from './utils/money.js';
-import '../data/cart-oop.js';
+import '../data/cart-class.js';
 
 export function renderAmazon(){
 updateCartQuantity();
